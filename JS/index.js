4fc7d6519095d8688de318175198e0fe6887db01
Nicolas-Guarino <nicolasguarino03@gmail.com>
2023-10-10 22:30:31 -0300

    Estilos Bootstrap

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+const toProduct = document.getElementById('toProduct');
+const toMoves = document.getElementById('toMoves');
+
+
+toProduct.addEventListener("click", function () {
+    window.location.href = "product.html";
+});
+
+toMoves.addEventListener("click", function () {
+    window.location.href = "moves.html";
+});
---
+++ JS/product.js
@@ -0,0 +1,5 @@
+
+document.getElementById("registerProd").addEventListener("click", function () {
+    // Redirige a moves.html al hacer clic en el botón
+    window.location.href = "moves.html";
+});
